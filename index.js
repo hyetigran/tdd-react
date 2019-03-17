eae131f7ff6ce9aba4991fc0a9e828b10e98fa4a
Tigran Asriyan <tigran.asriyan@gmail.com>
2019-03-18 01:27:13 +0400

    :tada: First commit

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports = (num) => {
+    if (num % 15 === 0) {
+        return 'FizzBuzz';
+    }
+};
+
+
+//} elseif (num % 5 ===0 ) {
+//return 'Buzz';
+//} else {return 'Fizz'}
+//return '${num}'
+//};
